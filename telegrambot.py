04793b4d52a5d3d7fd036a21cd49dc4253488c41
bigmanabel <bigmanabel@outlook.com>
2022-08-30 22:49:32 +0000

    Added these one

---
+++ telegrambot.py
@@ -0,0 +1,81 @@
+from telegram.ext.updater import Updater
+from telegram.update import Update
+from telegram.ext.callbackcontext import CallbackContext
+from telegram.ext.commandhandler import CommandHandler
+from telegram.ext.messagehandler import MessageHandler
+from telegram.ext.filters import Filters
+
+API_KEY = '5703743658:AAH9fh3vmiwMVISeLdBuUVGu06yxazgZSWg'
+
+
+updater = Updater(API_KEY, use_context=True)
+
+print("Starting bot...")
+
+
+def handle_message(update, context):
+    text = str(update.message.text).lower()
+
+    update.message.reply_text(f'Hi from telegram bot')
+
+
+
+def start(update: Update, context: CallbackContext):
+	update.message.reply_text(
+		"Hello sir, Welcome to the Bot.Please write\
+		/help to see the commands available.")
+
+def help(update: Update, context: CallbackContext):
+	update.message.reply_text("""Available Commands :-
+	/youtube - To get the youtube URL
+	/linkedin - To get the LinkedIn profile URL
+	/gmail - To get gmail URL
+	/geeks - To get the GeeksforGeeks URL""")
+
+
+def gmail_url(update: Update, context: CallbackContext):
+	update.message.reply_text(
+		"Your gmail link here (I am not\
+		giving mine one for security reasons)")
+
+
+def youtube_url(update: Update, context: CallbackContext):
+	update.message.reply_text("Youtube Link =>\
+	https://www.youtube.com/")
+
+
+def linkedIn_url(update: Update, context: CallbackContext):
+	update.message.reply_text(
+		"LinkedIn URL => \
+		https://www.linkedin.com/in/dwaipayan-bandyopadhyay-007a/")
+
+
+def geeks_url(update: Update, context: CallbackContext):
+	update.message.reply_text(
+		"GeeksforGeeks URL => https://www.geeksforgeeks.org/")
+
+
+def unknown(update: Update, context: CallbackContext):
+	update.message.reply_text(
+		"Sorry '%s' is not a valid command" % update.message.text)
+
+
+def unknown_text(update: Update, context: CallbackContext):
+	update.message.reply_text(
+		"Sorry I can't recognize you , you said '%s'" % update.message.text)
+
+
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('youtube', youtube_url))
+updater.dispatcher.add_handler(CommandHandler('help', help))
+updater.dispatcher.add_handler(CommandHandler('linkedin', linkedIn_url))
+updater.dispatcher.add_handler(CommandHandler('gmail', gmail_url))
+updater.dispatcher.add_handler(CommandHandler('geeks', geeks_url))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
+updater.dispatcher.add_handler(MessageHandler(
+	Filters.command, unknown)) # Filters out unknown commands
+
+# Filters out unknown messages.
+updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown_text))
+
+updater.start_polling()
